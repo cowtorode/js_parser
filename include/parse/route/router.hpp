fadec6840ed6c9920bcb4e1443e4da8522aecbfb
Cory Torode <cowtorode@gmail.com>
2025-07-01 11:10:31 -0400

    began routing

---
+++ include/parse/route/router.hpp
@@ -0,0 +1,18 @@
+//
+// Created by cory on 6/25/25.
+//
+
+#ifndef JSON_ROUTER_HPP
+#define JSON_ROUTER_HPP
+
+
+namespace json
+{
+	class router
+	{
+
+	};
+}
+
+
+#endif //JSON_ROUTER_HPP
---
+++ src/parse/route/router.cpp
@@ -0,0 +1,10 @@
+//
+// Created by cory on 6/25/25.
+//
+
+#include "parse/route/router.hpp"
+
+namespace json
+{
+
+}
